dfe9aa8f0c75dc8f47d593cf5a99c4ab26259d4b
Brian Dennis <briandennis321@gmail.com>
2016-07-21 22:42:20 -0500

    add a tad of styling to this ugly prototype

--- app/components/playlistsContainer.js
+++ app/components/playlistsContainer.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 function Playlist (props) {
   return (
-    <div>
-      <img src={props.image} height="100"/>
-      <a href={props.url}><h2>{props.name}</h2></a>
+    <div className="tile">
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={props.image}/>
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="content">
+            <a href={props.url}><h2>{props.name}</h2></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
