a75d8e9799ade42143939643f7f4cb07d0f3e876
Brian Dennis <briandennis321@gmail.com>
2016-07-24 17:40:44 -0500

    update settings

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const http = require('http');
-const router = require('./api/router');
-const swig = require('swig');
-const models = require('./models');
+const express   = require('express');
+const http      = require('http');
+const path      = require('path');
+const router    = require('./api/router');
+const swig      = require('swig');
+const models    = require('./models');
+const config     = require('./settings');
+const passport  = require('passport');
+const TwitterStrategy = require('passport-twitter');
 
 const port = process.env.PORT || 8000;
 const app = express(http);
+
+// configure passport
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, cb) {
+    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // Set static directory
 app.use(express.static('static'));
--- settings.js
+++ settings.js
@@ -1,9 +1,15 @@
 const settings = {};
 
+// Domain
+settings.HOST = process.env.HOST || 'http://localhost:8000';
+
 // DB
 settings.DB_NAME = process.env.DB_NAME || 'kastly';
 settings.DB_USERNAME = process.env.DB_USERNAME || 'briandennis';
 settings.DB_PASSWORD = process.env.DB_PASSWORD || '';
 
+// Twitter API keys
+settings.TWITTER_KEY = process.env.TWITTER_KEY || '';
+settings.TWITTER_SECRET = process.env.TWITTER_SECRET || '';
 
 module.exports = settings;
