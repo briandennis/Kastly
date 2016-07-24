913712c11f3e47f3db6115551211737127ead231
Brian Dennis <briandennis321@gmail.com>
2016-07-24 15:18:07 -0500

    add models folder and settings file

---
+++ settings.js
@@ -0,0 +1,6 @@
+const settings = {};
+
+// DB
+settings.DB_NAME = process.env.DB_NAME || 'kastly';
+settings.DB_USERNAME = process.env.DB_USERNAME || 'briandennis';
+settings.DB_PASSWORD = process.env.DB_PASSWORD || '';
