a5f0f0eca2e59401bb3270ea4953786e85142310
Brian Dennis <briandennis321@gmail.com>
2016-08-02 22:33:04 -0500

    add user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
-var Schema = require('mongoose').Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const findOrCreate = require('mongoose-findorcreate');
+
+const UserSchema = new Schema({
+  name: { type: String, default: '' },
+  email: { type: String, default: '' },
+  username: { type: String, default: '' },
+  twitterId: { type: String, default: '' }
+});
+
+UserSchema.plugin(findOrCreate);
+
+mongoose.model('User', UserSchema);
