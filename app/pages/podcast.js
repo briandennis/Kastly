0233b68bdb709460547f83eb3856a1f6aa697d1e
Brian Dennis <briandennis321@gmail.com>
2016-08-14 20:12:04 -0500

    add api service

--- app/pages/podcast.js
+++ app/pages/podcast.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Podcast extends React.Component {
 
   render() {
-    return <div>Test!</div>;
+    return (
+      <div>
+        
+      </div>
+    );
   }
 
 }
---
+++ app/providers/api.service.js
@@ -0,0 +1,5 @@
+const Podcast = {
+  get( podcastId ) {
+    
+  }
+};
