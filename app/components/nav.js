affc287aa4d139948f7599b7005c4a7a52de50eb
Brian Dennis <briandennis321@gmail.com>
2016-08-27 14:01:18 -0500

    add log in button to new nav

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,12 +1,33 @@
 import React from 'react'
 
-const Nav = () => {
+const Nav = (props) => {
+
+  let loginButton;
+  if (props.loggedIn) {
+    loginButton = (
+      <a className="button is-light" href="/logout">
+        Log Out
+      </a>
+    );
+  } else {
+    loginButton = (
+      <a className="button is-primary" href="/login/twitter">
+        <span style={{marginRight: '10px'}} className="icon">
+          <i className="fa fa-twitter"></i>
+        </span>Log in
+      </a>
+    );
+  }
+
   return (
     <div className="nav">
       <div className="nav-left">
         <div className="nav-item is-brand">
           <h1 className="navTitle">Kastly</h1>
         </div>
+      </div>
+      <div className="nav-right">
+        {loginButton}
       </div>
     </div>
   );
