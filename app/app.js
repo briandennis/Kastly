8743583199823130edb9de01bc6c77a01a23d3b1
Brian Dennis <briandennis321@gmail.com>
2016-08-27 14:45:30 -0500

    fix routing

--- app/app.js
+++ app/app.js
@@ -1,15 +1,8 @@
 import React from 'react';
-
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import { connect } from 'react-redux';
-
 import * as Actions from './actions';
 
 import Nav from './components/nav';
-import Discover from './pages/discover';
-import Home from './pages/home';
-import Podcast from './pages/podcast';
-import Profile from './pages/profile';
 
 class App extends React.Component {
 
@@ -27,14 +20,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path="/" component={NavComponent}>
-          <Route path="/" component={Home}></Route>
-          <Route path="/podcast/:castId" component={Podcast}></Route>
-          <Route path="/discover" component={Discover}></Route>
-          <Route path="/profile/:userId" component={Profile}></Route>
-        </Route>
-      </Router>
+      <div>
+        <Nav />
+        {this.props.children}
+      </div>
     );
   }
 }
--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
 import App from './app';
+import Discover from './pages/discover';
+import Home from './pages/home';
+import Podcast from './pages/podcast';
+import Profile from './pages/profile';
 
 const entryNode = document.getElementById('app');
 
@@ -12,6 +17,13 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home}></IndexRoute>
+        <Route path="podcast/:castId" component={Podcast}></Route>
+        <Route path="discover" component={Discover}></Route>
+        <Route path="profile/:userId" component={Profile}></Route>
+      </Route>
+    </Router>
   </Provider>,
   entryNode);
