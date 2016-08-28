e171e1be44be66abe590ce142bafa0b0c338b935
Brian Dennis <briandennis321@gmail.com>
2016-08-28 11:54:11 -0500

    fix new schemas

--- models/episode.js
+++ models/episode.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Podcast = require('./podcast');
-const findOrCreate = require('mongoose-findorcreate');
 
 const EpisodeSchema = new Schema({
-  podcast: { type: Podcast },
+  podcast: { type: Schema.ObjectId, ref: Podcast },
   description: { type: String, default: '' },
   title: { type: String, default: '' },
   date: { type: Date },
