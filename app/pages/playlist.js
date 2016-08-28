5058ff309e64400422a2ff035ec7d7dfce8114c2
Brian Dennis <briandennis321@gmail.com>
2016-08-27 22:26:29 -0500

    add functionality to playlist page

--- app/pages/playlist.js
+++ app/pages/playlist.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+import { playlistService } from './../providers/api.service';
+
 class Playlist extends React.Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      playlist: null
+    };
+  }
+
   componentWillMount () {
-    
+    playlistService.get(this.props.params.playlistId)
+      .then( (playlist) => {
+        this.setState({
+          playlist
+        });
+      })
+      .catch( (error) => console.log(error));
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.playlist}
+      </div>
+    );
   }
 }
 
