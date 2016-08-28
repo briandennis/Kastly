dddb76c27161466be9e9300afc66334c40d7ee71
Brian Dennis <briandennis321@gmail.com>
2016-08-27 20:28:31 -0500

    add comment schema

---
+++ models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const findOrCreate = require('mongoose-findorcreate');
+
+const CommentSchema = new Schema({
+  userId: { type: String, default: '' },
+  text: { type: String, default: '' },
+  timestam: { type: Number}
+});
+
+CommentSchema.plugin(findOrCreate);
+
+module.exports = mongoose.model('Comment', CommentSchema);
--- models/playlist.js
+++ models/playlist.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose    = require('mongoose');
+const Schema      = mongoose.Schema;
+const Comment     = require('./comment');
 const findOrCreate = require('mongoose-findorcreate');
 
 const PlaylistSchema = new Schema({
   title: { type: String, default: '' },
   description: { type: String, default: '' },
-  content: { type: Array, default: [] },
+  content: { type: [String], default: [] },
   comments: { type: String, default: '' },
   twitterId: { type: String, default: '' }
 });
