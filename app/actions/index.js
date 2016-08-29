5fdcfd670bda4dd39b04d1158973bf7a5269cf95
Brian Dennis <briandennis321@gmail.com>
2016-08-28 20:40:34 -0500

    add actions for playlists

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,13 +2,34 @@ export function fetchingUser() {
   return { type: 'FETCH_USER' };
 }
 
-export function setUser( success, user, err ) {
+export function setUser(success, user, error) {
   if ( success ) {
     return {
       type: 'FETCH_USER_SUCCESS',
       user: Object.assign({}, user, { id: user._id })
     };
   } else {
-    return { type: 'FETCH_USER_FAILURE' };
+    return {
+      type: 'FETCH_USER_FAILURE',
+      error
+    };
+  }
+}
+
+export function fetchingPlaylists() {
+  return { type: 'FETCH_PLAYLIST' };
+}
+
+export function setPlaylists(success, playlists, error) {
+  if (success) {
+    return {
+      type: 'FETCH_PLAYLISTS_SUCCESS',
+      playlists:
+    };
+  } else {
+    return {
+      type: 'FETCH_PLAYLISTS_ERROR',
+      error
+    };
   }
 }
