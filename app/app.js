9aad5ea9f41a8385431670ea07ca9af4edad3a64
Brian Dennis <briandennis321@gmail.com>
2016-08-28 20:50:59 -0500

    fix imports in app component

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { setUser, fetchingUser, setPlaylists, fetchingPlaylists } from './actions';
 
