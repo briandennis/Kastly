8cf41bfe6a31edcb22ff855af3dfe7111726e5fb
Brian Dennis <briandennis321@gmail.com>
2016-08-29 23:55:54 -0500

    finish playlist component

--- app/components/playlistImage.js
+++ app/components/playlistImage.js
@@ -13,7 +13,11 @@ const PlaylistImage = (props) => {
       }
     });
 
-    let image;
+    let image = (
+      <div height={`${props.size}px`} width={`${props.size}px`}>
+        <img src="/static/img/defaultCastIcon.png" height="100%" width="100%"/>
+      </div>
+    );
 
     switch (uniqueLinks.length) {
       case 1:
@@ -48,6 +52,12 @@ const PlaylistImage = (props) => {
       );
     }
   }
+
+  return (
+    <div>
+      {getUnique(props.episode)}
+    </div>
+  );
 }
 
 export default PlaylistImage
