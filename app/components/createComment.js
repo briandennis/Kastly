473e36515f730e34eb64484e0ef14b9a04cc7561
Brian Dennis <briandennis321@gmail.com>
2016-08-30 17:09:01 -0500

    change create comment to class;

--- app/components/createComment.js
+++ app/components/createComment.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function CreateComment (props) {
+class CreateComment extends React.Component {
 
   const validate = (e) => {
     if (e.preventDefault) e.preventDefault();
@@ -10,18 +10,20 @@ function CreateComment (props) {
     console.log(content);
   }
 
-  return (
-    <div>
-      <form onSubmit={validate}>
-        <label className="label">New Comment</label>
-        <p className="control">
-          <textarea name="content" className="textarea" placeholder="Textarea"></textarea>
-        </p>
-        <div className="control">
-          <button className="button is-primary">Submit</button>
-        </div>
-      </form>
-    </div>
+  render (
+    return (
+      <div>
+        <form onSubmit={this.validate}>
+          <label className="label">New Comment</label>
+          <p className="control">
+            <textarea name="content" className={`textarea ${this.props.class}`} placeholder="Textarea"></textarea>
+          </p>
+          <div className="control">
+            <button className="button is-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
   );
 }
 
