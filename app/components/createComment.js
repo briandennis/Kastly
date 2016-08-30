e756d331ba4780242316342e7322a4bd36521068
Brian Dennis <briandennis321@gmail.com>
2016-08-30 17:19:03 -0500

    update create comment

--- app/components/createComment.js
+++ app/components/createComment.js
@@ -2,22 +2,53 @@ import React from 'react';
 
 class CreateComment extends React.Component {
 
-  const validate = (e) => {
+  constructor () {
+    super();
+
+    this.state = {
+      error: false,
+      content: ''
+    };
+
+    this.validate = this.validate.bind(this);
+    this.updateForm = this.updateForm.bind(this);
+  }
+
+  validate (e) => {
     if (e.preventDefault) e.preventDefault();
 
-    const content = e.target.content.value;
+    if (this.content && this.content.length < 250) {
+      this.props.submitComment(this.content);
+    } else {
+      this.setState({ error: true });
+    }
+  }
 
-    console.log(content);
+  updateForm (e) {
+    this.setState({ content: e.target.value });
   }
 
   render (
+
     return (
       <div>
         <form onSubmit={this.validate}>
           <label className="label">New Comment</label>
           <p className="control">
-            <textarea name="content" className={`textarea ${this.props.class}`} placeholder="Textarea"></textarea>
+            <textarea name="content"
+                      className={
+                        this.state.error
+                        ? 'textarea is-danger'
+                        : 'textarea'}
+                      value={this.state.content}
+                      onChange={this.updateForm}>
+            </textarea>
           </p>
+          {
+            this.state.error
+            ? <span className="help is-danger">This email is invalid</span>
+            : ''
+          }
           <div className="control">
             <button className="button is-primary">Submit</button>
           </div>
