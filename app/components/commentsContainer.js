3a5ee216ba55114fbd008025127a3d3fed3907d3
Brian Dennis <briandennis321@gmail.com>
2016-08-30 16:45:11 -0500

    upcate comment container with new components

--- app/components/commentsContainer.js
+++ app/components/commentsContainer.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { PlaylistService } from './../providers/api.service';
 
 import Comment from './comment';
+import CreateComment from './createComment';
 
 function CommentsContainer (props) {
 
   const comments = props.comments.map( (comment, index) => {
-    return
-  })
+    return <Comment key={index} comment={comment} />;
+  });
 
   return (
     <div className="playlistContainer">
       <p>Hello, world!</p>
-      <p> {props.playlist.content.length} </p>
+      <div>
+        {comments}
+      </div>
+      <div>
+        <CreateComment />
+      </div>
     </div>
   );
 }
