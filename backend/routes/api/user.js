2d89cc6bc858684e08ae12e485bbce1433a5f008
Brian Dennis <briandennis321@gmail.com>
2016-08-30 14:53:20 -0500

    update user route to return non logged in users

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,10 +1,17 @@
+const User = require('./../../models/user');
+
 module.exports = function (req, res, next) {
   if ( !req.params.userId ) {
-    if (req.session.user) {
-      console.log('Sending user: ' + req.session.user._id);
+    if (req.session.user && req.session.user._id === req.params.userId) {
       res.json(req.session.user);
     } else {
-      res.status(200).json(null);
+      User.findOne({ _id: req.params.userId })
+        .then( (user) => {
+          res.status(200).json(user);
+        })
+        .catch( (error) => {
+          res.status(500).send();
+        });
     }
   }
 };
