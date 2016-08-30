8586855b4e6ed2f19b90ca381c00fc334aa54cb7
Brian Dennis <briandennis321@gmail.com>
2016-08-30 12:12:19 -0500

    add playlists page component

---
+++ app/pages/playlists.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Playlists extends React.Component {
+  constructor () {
+    super();
+
+    this.state = {
+      playlists: [];
+    }
+  }
+
+  componentWillMount () {
+    
+  }
+}
