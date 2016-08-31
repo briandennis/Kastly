8ea184060b1769c0959cea75a3baa50090c4542e
Brian Dennis <briandennis321@gmail.com>
2016-08-31 17:50:31 -0500

    rewrite star component

--- app/components/likes.js
+++ app/components/likes.js
@@ -1,28 +1,57 @@
 import React from 'react';
 
-function Likes (props) {
+class Likes extends React.Component {
 
-  return (
-    <div className="like">
-    <div>
-      <span className="icon like"
-            onClick={
-              props.loggedIn
-              ? props.toggle
-              : ''
-            }>
-        <i className={
-              props.loggedIn && !props.liked
-              ? 'fa fa-star liked'
-              : 'fa fa-star liked'
-            } />
-      </span>
-    </div>
-    <div className="likeText">
-      {props.liked ? 'true' : 'false'}
-    </div>
-    </div>
-  );
+  constructor() {
+    super();
+
+    this.state = {
+      hover: false;
+    }
+
+    this.mouseOver = this.mouseOver.bind(this);
+    this.mouseOut = this.mouseOut.bind(this);
+  }
+
+  mouseOver () {
+      this.setState({hover: true});
+  }
+
+  mouseOut () {
+      this.setState({hover: false});
+  }
+
+  mapHoverToIconClass (liked, hover) {
+    if (liked) {
+      if (hover) {
+        return 'fa fa-star-o';
+      }
+      return 'fa fa-star';
+    } else {
+      if (hover) {
+        return 'fa fa-star';
+      }
+      return 'fa fa-star-o';
+    }
+  }
+
+  render () {
+    // get icon className
+    let iconClass = 'fa fa-star';
+    if (this.state.props.loggedIn) {
+      iconClass = mapHoverToIconClass(this.state.props.liked, this.state.hover);
+    }
+
+    return (
+      <div className="like">
+        <span className="icon">
+          <i className={iconClass}></i>
+        </span>
+      </div>
+    );
+  }
+
+
 }
 
 export default Likes;
